75c71a3f909375dab9a60e5237dcc07dc79a9acb
Aliston <changliseng0903@gmail.com>
2024-06-26 13:59:26 +0800

    only make change to add element

--- scripts/content.js
+++ scripts/content.js
@@ -1,8 +1,17 @@
 ( () => {
-    console.log( 'start' );
-    let elements = null;
+    const keywords = [ "VNL" ];
     const observer = new MutationObserver( ( mutations ) => {
-        blockYoutubeVideos( [ "VNL" ] );
+        mutations.forEach( ( mutation ) => {
+            if ( mutation.type === "childList" ) {
+                mutation.addedNodes.forEach( ( addedNode ) => {
+                    if ( addedNode instanceof Element && addedNode.matches( 'ytd-rich-item-renderer' ) ) {
+                        // Only make change if the new element is a grid row
+                        console.log( 'New grid row element added:', addedNode );
+                        blockYoutubeVideos( addedNode, keywords );
+                    }
+                } );
+            }
+        } );
     } );
     observer.observe( document.body, {
         childList: true,
@@ -10,14 +19,12 @@
     } );
 } )();
 
-function blockYoutubeVideos( keywords ) {
-    const videoElements = document.querySelectorAll( "ytd-rich-item-renderer" );
-    videoElements.forEach( ( videoElement ) => {
-        const titleElement = videoElement.querySelector( "#video-title" );
-        const titleText = titleElement.textContent;
-        if ( keywords.some( ( keyword ) => titleText.includes( keyword ) ) ) {
-            // make the video element blank
-            videoElement.style.visibility = "hidden";
-        }
-    } );
+function blockYoutubeVideos( videoElement, keywords ) {
+    const titleElement = videoElement.querySelector( "#video-title" );
+    const titleText = titleElement.textContent;
+    if ( keywords.some( ( keyword ) => titleText.includes( keyword ) ) ) {
+        // make the video element blank
+        // videoElement.style.visibility = "hidden";
+        videoElement.style.backgroundColor = "lightblue";
+    }
 }
