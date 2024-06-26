32a50ea7aa00f297e6408c630bf7cc21732e0e81
Aliston <changliseng0903@gmail.com>
2024-06-26 22:31:02 +0800

    make it homepage certain

--- scripts/content.js
+++ scripts/content.js
@@ -4,7 +4,8 @@
         mutations.forEach( ( mutation ) => {
             if ( mutation.type === "childList" ) {
                 mutation.addedNodes.forEach( ( addedNode ) => {
-                    if ( addedNode instanceof HTMLDivElement && addedNode.id === 'dismissible' ) {
+                    // if ( addedNode instanceof HTMLDivElement && addedNode.id === 'dismissible' ) {
+                    if ( addedNode instanceof Element && addedNode.matches( 'ytd-rich-item-renderer' ) ) {
                         // Only make change if the new element is a grid row
                         console.log( 'New grid row element added:', addedNode );
                         blockYoutubeVideos( addedNode, keywords );
@@ -24,7 +25,7 @@ function blockYoutubeVideos( videoElement, keywords ) {
     const titleText = titleElement.textContent;
     if ( keywords.some( ( keyword ) => titleText.includes( keyword ) ) ) {
         // make the video element blank
-        // videoElement.style.visibility = "hidden";
-        videoElement.style.backgroundColor = "lightblue";
+        videoElement.style.visibility = "hidden";
+        // videoElement.style.backgroundColor = "lightblue";
     }
 }
