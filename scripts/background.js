90527d9fed8320049bb24d041b4e66a0cceccf2b
changliseng <changliseng0903@gmail.com>
2024-07-01 13:02:50 +0800

    test background

--- scripts/background.js
+++ scripts/background.js
@@ -1,9 +1,11 @@
 chrome.tabs.onUpdated.addListener( ( tabId, tab ) => {
     if ( tab.url && tab.url.includes( "youtube.com" ) ) {
-        console.log( "Background" );
-        chrome.tabs.sendMessage( tabId, {
-            type: "NEW",
-            videoId: urlParameters.get( "v" ),
-        } );
+        // console.log( "It's a youtube page" );
+        // chrome.tabs.sendMessage( tabId, {
+        //     type: "NEW",
+        //     videoId: urlParameters.get( "v" ),
+        // } );
+    } else {
+        // console.log( "It's not a youtube page" );
     }
 } );
